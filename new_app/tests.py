06c961949e2a95c3c9b1e68daf8938e11e17c69a
Inferato <infaustmager@gmail.com>
2024-07-04 20:57:36 +0300

    fix: fix lint

--- new_app/tests.py
+++ new_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .views import do_math
 
-# Create your tests here.
+
+class TestMath(TestCase):
+
+    def test_correct(self):
+        a = 5
+        b = 2
+        result = do_math(a,b)
+        self.assertEqual(result, 7)
+
+    def test_wrong(self):
+        a = 5
+        b = 2
+        result = do_math(a,b)
+        self.assertEqual(result, 8)
